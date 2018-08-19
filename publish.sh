c7363e0af22410916bb4723efda008ad44c7bf6d
Rafael Araujo <rafael.araujo@endel.com.br>
2018-08-19 15:48:28 +1200

    Add: env files test on publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ ! -f lvl-rest/.env ]; then
+    echo "Create lvl-rest.env file"
+    exit;
+fi
 cd lvl-rest && php composer.phar install
 php artisan migrate
 cd ../ngjs/build && npm run-script publish
