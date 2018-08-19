9fcc5a931276d074e096873459387de6b176f095
Rafael Araujo <rafael.araujo@endel.com.br>
2018-08-19 14:36:17 +1200

    Add: add seed

---
+++ lvl-rest/database/seeds/PostTableSeeder.php
@@ -0,0 +1,172 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $posts = [
+            [
+                'title'        => 'Where To Buy Vinyl In Auckland',
+                'content'      => 'Forget about tape players and CD’s, today’s music is all about streaming—or is it? There’s one form of music that’s been making it’s way back onto the scene and we love it. Vinyl (that’s records for you peeps you can’t remember) are those black, flat, round gems that you place on a player and listen as musician’s tune play out in all their scratchy goodness. And so, whether you’re a collector or are simply feeling a touch nostalgic, it’s time to get your hands on some good old, warm-toned vinyl. For the love of the LP, we bring you Auckland’s best vinyl stores. 
+Real Groovy 
+
+Auckland City 
+
+You would be hard pressed to find an Aucklander who hasn’t, at some point in their lives, ventured into the doors of Real Groovy.  If you’re into a bit of movin’ and groovin’ to some vinyl, then this is your essential first stop. If you’re not sure what vinyl’s you need in your life, they even have a handy ‘recommended vinyl’ section to help you out.
+Southbound Records
+
+Eden Terrace
+
+Five years ago, Southbound Records opened their doors on up-and-coming Symonds St and they’ve never looked back. Stocking New Zealand’s largest range of new vinyl, they have also hosted ‘Record Day’, which saw people spilling into the streets. Who said vinyl is out? 
+Conch Kitchen & Bar
+
+Ponsonby
+
+Sitting pretty on Ponsonby Road, Conch Kitchen & Bar is a South American-inspired kitchen that began as a market stall in Aotea Square. They then moved into a small shop on K\' Road and while they are probably better known for their food and drinks, they stay true to their record store roots. You can head to their website for a curated selection of records. 
+Marbecks
+
+Auckland City 
+
+If you’re looking for history, then Marbecks has it in spades. It was set up in 1934 and has been passed down generation to generation a whopping four times. Situated in Queens Arcade, it stocks a wide range of vinyl gems, including exclusive imports, and the staff have an incredible knowledge of all things music. 
+Flying Out
+
+Auckland City 
+
+While they have plenty of vinyl for sale via their website, it’s worth heading to Flying Out’s Pitt St store for the total experience. Selling independent record labels Flying Nun Records and Arch Hill Recordings, they also stock a heap of other titles. 
+Musiquariam
+
+Mt Eden
+
+This Dominion Road record store may look unassuming from the outside, but has plenty of vinyl on offer inside. The shop has been around for 12 years and has a strong supply of New Zealand musicians including Split Enz, and other kiwi musicians from the 60s and 70s. 
+Alien Records
+
+New Lynn
+
+Considered a spot filled with ‘music nerds’ this is the place where people really know their stuff. Located in a random warehouse down a driveway in New Lynn, you will find the hidden music store. 
+Want more?
+
+    The Best Spots For Live Music In Auckland
+    8 Places To Dance The Night Away
+    27 Reasons To Stay In On A Saturday Night
+
+Image credit: Jamakassi',
+                'is_published' => 1,
+                'user_id'=> 1
+            ],
+
+            [
+                'title'        => 'The Ultimate Luxury Weekend Escapes From Auckland',
+                'content'      => '![Image](https://tul.imgix.net/content/article/matuka-lodge.jpg)
+                
+                Each and every one of us has our own way of de-stressing and unwinding. Some of us like to treat ourselves to some retail therapy whereas some of us are perfectly content with a pint of ice-cream and a spoon. Although those do the trick most of the time, there are times when it all gets a bit too much and you just need to get away from the madness. A weekend retreat is what we’re talking about—but not just any retreat, a full-on luxurious escape where you live a couple days like royalty because you deserve it!
+
+Here’s a list of the ultimate luxury weekend escapes from Auckland.
+Lantern House
+
+Waiheke Island
+
+Take a quick ferry to another island and spend your weekend at the modern and luxurious spot. Waiheke Island is not only renowned for their wineries but also the serene environment and clean crisp air this land offers. The Lantern House has made a name for itself for being one of the most luxurious forms of accommodation in New Zealand through many awards over the years. The private resort is tucked away within the hills of Waiheke Island, overlooking the crystal blue waters of Onetangi and is an idyllic spot for a much-needed escape. The interior has a Zen-like vibe of balanced symmetry from the furniture to the layout and will bring some peace to your mind.
+Eagles Nest
+
+The Bay of Islands
+
+Fly away from your worries to Eagles Nest in the Bay of Islands as this retreat spot is everything you want in a mini-getaway and more. Eagles Nest has five ultra-luxurious villas, nestled in an island away from the big crowds. Although all five villas are out of this world, we just had to talk more about the Rahimoana villa as it\'s almost too good to be true. They take hospitality to a new level with the facilities they provide. A queen-sized bed and ensuite bathroom? Oh no! Eagles Nest goes so faras giving you your own private movie theatre, private beach and bushwalk.
+Mahu Whenua Ridgeline Suites
+
+Wanaka
+
+There is that common joke which states that you know when someone is from Wanaka as they don’t stop talking about it. But to be fair, Wanaka is truly stunning and we would be the same! Mahu Whenua Ridgeline Suites gives stunning views of Lake Wanaka and from the moment you walk in to this private destination, you know you’re in for a royal treatment. The grand Homestead offers the top facilities in terms of accommodation but also the best on-site activities, you’ll never have to leave! They have the essential spa treatments and house gym but they even go as far as offering activities such as horse riding, yoga, cooking courses and heli-skiing!
+Annandale Coastal Farm Escape & Luxury Villa Collections
+
+Pigeon Bay
+
+Situated at the edge of the Canterbury region, Pigeon Bay is the definition of the phrase ‘off the grid’. Annandale Coastal Farm Escape & Luxury Villa Collections show the best of the rural life New Zealand has to offer. With a coastline that stretches for 10km and a private farm, Annandale is famous for their farm-to-table chef experience which makes this destination ideal for the foodies out there. They only use the freshest and the best ingredients to provide their guests with premium quality food and paired with stellar service for a dining experience of a lifetime.
+Acacia Cliffs Lodge
+
+Lake Taupo
+
+Just under a four-hour drive from Auckland, Lake Taupo has become a common destination amongst Aucklanders for a wee escape. Have a yolo moment and take a leap of faith at the Lake Taupo Bungy and then relax at the Acacia Cliffs Lodge. Nestled up against the hills of Taupo, you get a breath-taking view of the sparkling waters of Lake Taupo. Wake up to the rising golden sunrise and make your way downstairs you get a complimentary gourmet breakfast fit for a champion. You even get to enjoy this on their outdoor deck that soaks up all the rays of the sun and gets you feeling like you’re living your best life.
+Cape Kidnappers Golf Course Robertson Lodges
+
+Hawkes Bay
+
+Nothing says boujee like a golf trip at the country club. Cape Kidnappers Golf Course is recognised as one of the best golf courses in the entire world for its lush greenery, smooth fields and top of the class facilities. Take your golf trip to the next level by going all out and booking the hilltop suite. Located at the highest point of the lodge, you’re bound to get the best view of the never-ending fields of the most beautiful golf course in the world. Robertson Lodges also offers different activities like outdoor picnics and horse riding on their spacious farm.
+
+The Ultimate Luxury Weekend Escapes From Auckland
+Treetops Lodge & Estate
+
+Rotorua
+
+When we say Rotorua, the first thing that pops into a lot of people’s minds would be the infamous luge rides. What a lot of people don’t seem to know is that Rotorua has the means to provide fine quality luxury! The Treetops Lodge & Estate is more than just a luxury experience as you get the opportunity to get immersed in Maori culture. From traditional Maori food and a Heritage Trail, The Treetops Lodge & Estate offer an authentic and educational experience which makes this luxury form of accommodation special. For those that really want a private retreat, they offer a pheasant cottage located 1.4km away from the main lodge. The ambience is tranquil, rustic yet classy with a wood burning fireplace that you can get cosy beside to.
+Jacks Point Escape
+
+Queenstown
+
+Hidden under the shadows of the iconic mountains of the South Island, Jacks Point Escape is everything you want in a luxury weekend escape and more. Don’t think that a weekend trip to Queenstown is unrealistic, just spare a few hours, hop on a plane and voila! Jacks Point Escape is a holiday house that is modern but homely, you’ll never want to leave. From the inside and out, this holiday home surprises visitors with something new every corner they turn. Floor to ceiling windows, show-stopping views, and wood-burning fireplace both inside and on the outdoor deck, the list just goes on.
+
+The Ultimate Luxury Weekend Escapes From Auckland
+Matuka Lodge
+
+Twizel
+
+Near Mount Cook National Park is the charming little town of Twizel, offering the best of New Zealand’s scenery. Twizel is better known for being the on-shoot location for the critically acclaimed blockbuster hit The Lord of the Rings but it’s also a haven for a weekend escape. Matuka Lodge has a north-facing outdoor deck where you can just spend hours marvelling over the beauty our country has to offer under the watchful gaze of the Southern Alps.',
+                'is_published' => 1,
+                'user_id'=> 1
+            ],
+
+            [
+                'title'        => '8 Awesome Auckland Charities To Get Behind',
+                'content'      => '![Image](https://tul.imgix.net/content/article/awesome-auckland-charities-to-get-behind.jpg?auto=format,compress&w=740&h=486&fit=crop&crop=edges)
+                
+                Are you looking to give back to the community? Interested in squeezing in a bit of purpose-searching in your weekly timetable? Or simply want to stretch your tax dollar that little bit further? Then investing your time and/or money into a local Auckland charity is the way to go. Luckily, our city is home to a number of worthwhile charities that are always open to a helping hand or a donation.
+
+Here are seven awesome Auckland charities and non-for-profits to get behind.
+Everybody Eats
+
+Food wastage is a huge problem and founder of Everybody Eats Nick Loosley decided to do something about it! Every Monday, Everybody Eats joins forces with Gemmayze Street and make the most of the food that would’ve otherwise gone to waste. They cook up a feast for everyone to enjoy and you pay how much or how little you want, if at all. For every $10 they make, they can provide three restaurant quality, three-course meals for the people in need. So, next time you’re brainstorming a place to eat on a Monday, goodness at Gemmayze St is waiting for you!
+Good Works Trust
+
+Just like their name says, Good Works Trust is a charitable trust on the North Shore, dedicated to doing nothing but good for their community! They do all sorts of things to make the world a better place for everyone, such as delivering emergency food parcels to families in need, Christmas food shops so everyone has the chance to get festive and even family events at Parakai Hot Pools! Good Works Trust is all about positivity and they show that through their actions that focus on enhancing the raw and priceless things in life such as family. To get involved and receive further information, click here.
+Auckland City Mission
+
+Everybody has their own story and every story has strong value and meaning. This is exactly what Auckland City Mission embody as they care for each individual no matter what their circumstances are. Whether you’re a struggling addict, homeless, abuse victim or an isolated elderly, Auckland City Mission is there to help. Their services include health care centers, crisis care centers, distribution services, elderly services, homeless services and social detoxification services and provide effective advocacy. They are using every fiber of their being to make a difference to Auckland society and you can too by giving a donation or volunteering at their events.
+Eat My Lunch
+
+Children need to spend their free time fuelled for the playground and fields and Eat My Lunch are giving all Kiwi kids the energy and nutrients they need. Every lunch you buy from Eat My Lunch, they give one away to a kid who really needs it while filling your own tummy too! These lunches are both nutritious, filling and super yum so it’s a win-win situation where everybody walks away happy!
+Bellyful
+
+Bellyful is a charity where volunteers get together every month and cook up a feast—otherwise known as a Cookathon—and deliver it to families in need, particularly those who are struggling to adjust with a new-born baby or ill family member. With a number of locations spread around Auckland, you can either offer them a small financial donation so they have the supplies to work their magic in the kitchen with. Either that or you could really get amongst it and be a cook in their next Cookathon!
+Compost Collective
+
+Speaking of waste, we have to give a shout out to the team at The Compost Collective! According to their website, half of the rubbish that New Zealand households are actually compostable and food waste alone comes to 90,000 tonnes each year! Nobody creates waste with the intention of harming the planet, most of us just aren’t knowledgeable when it comes to this sort of thing! Well, fear no more as The Compost Collective is here to educate and show us the best ways to make the most out of our waste and save further damage to the environment. Remember that giving back to the community isn’t only about the people, we have to look after Mother Nature too!
+Cure Kids
+
+Cure Kids are a charitable organisation who focus on finding more and more medical research and information on diseases that have a huge impact on children. Not sure if you all know this but medical research is EXPENSIVE which is why our donations can make a difference! Money isn’t the only way to help Cure Kids, you can even volunteer at one of their many events held all around the country including Auckland, where you show the children of New Zealand a good time!
+SPCA
+
+We can\'t forget about our furry little friends, they need love and care too! The SPCA dedicate to caring for the animals that are in need. Animals that have no homes, that have been abused and are living with illnesses are taken under the wing of th SPCA to rehabilitate, recover and hopefully find a secure place where they get to call home. There are lots of different ways you could show your support to the animal charity organisation. Donations are one way to help but adopting a pet from one of their shelters around Auckland is the way to go. This way you both have new companions!',
+                'is_published' => 0,
+                'user_id'=> 1
+            ]
+        ];
+
+        array_map(function ($post) {
+//            DB::collection('posts')->insert([
+//                'title'        => $post->title,
+//                'content'      => $post->content,
+//                'user_id'      => 1,
+//                'is_published' => $post->is_published
+//            ]);
+
+            App\Post::create($post);
+        }, $posts);
+    }
+}
